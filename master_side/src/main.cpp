726e5518204ae1f355c003100b3efb0b07fe4cfc
LoeffelEliott <100429864+LoeffelEliott@users.noreply.github.com>
2026-06-04 10:55:14 +0200

    let radio setting by default

--- master_side/src/main.cpp
+++ master_side/src/main.cpp
@@ -40,11 +40,6 @@ void setup() {
   Serial.begin(9600);
   Serial.print(F("[SX1262] Initializing ... "));
 
-  float freq = 2400;
-  float bw   = 406.25;
-  float sf   = 7;
-  float cr   = 5;
-
   int radio_state = radio.begin();
   if (radio_state == RADIOLIB_ERR_NONE) {
     Serial.println(F("success!"));
